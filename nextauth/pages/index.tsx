42ec61ea51fa55efe089e4321b1de4cbc1ae15a4
helton-quintans <helton.quit@gmail.com>
2022-12-10 22:45:47 -0300

    feat: input de email e senha e seus estados

--- nextauth/pages/index.tsx
+++ nextauth/pages/index.tsx
@@ -1,14 +1,18 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+      <input type="email" name="" id="" />
+      <input type="password" name="" id="" />
     </div>
   );
 };
