0daa0eca79c17d3a94644e14b85aca5755f8144f
helton-quintans <helton.quit@gmail.com>
2022-12-10 22:56:02 -0300

    styles: login

--- nextauth/pages/index.tsx
+++ nextauth/pages/index.tsx
@@ -5,24 +5,32 @@ import { useState } from "react";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = () => {
     const data = {
       email,
       password,
-    } 
+    };
 
-    console.log(data)
-
-  }
+    console.log(data);
+  };
 
   return (
     <form onSubmit={handleSubmit} className={styles.container}>
-      <input type="email" value={email} id="" onChange={e => setEmail(e.target.value)} />
-      <input type="password" value={password} id="" onChange={e => setPassword(e.target.value)}/>
+      <input
+        type="email"
+        value={email}
+        id=""
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        id=""
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button type="submit">Entrar</button>
     </form>
   );
